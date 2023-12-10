b9ad11a918fe2fb6e2b56f53185929032f3e09b4
damianolszewski <44201801+damianolszewski@users.noreply.github.com>
2023-12-10 15:59:42 +0100

    Update START.bat

--- START.bat
+++ START.bat
@@ -3,51 +3,19 @@ setlocal
 
 :: Define the game executable and repository URL
 set "gameExecutable=LethalCompany.exe"  :: Replace with your game's executable name
-set "repositoryURL=https://github.com/username/repository.git"  :: Replace with your repository URL
+set "repositoryURL=https://github.com/damianolszewski/PaczkaDoLethala.git"  :: Replace with your repository URL
 set "gameDirectory=%CD%"  :: Assumes the script is in the game directory
 set "gitExecutable=git"
 
 :: Check if Git is installed by checking the version
 git --version >nul 2>&1
-if true==true (
-    echo Git is not installed, installing Git...
-
-    :: Assuming gitinstaller.exe is in the same directory as this script
-	if not exist "%installerName%" (
-		echo Error: Git installer "%installerName%" not found.
-		exit /b 1
-	)
-
-    (
-        echo [Setup]
-        echo Lang=default
-        echo Dir=%installDir%
-        echo Group=Git
-        echo NoIcons=0
-        echo SetupType=default
-        echo Components=icons,ext\reg\shellhere,assoc,assoc_sh
-        echo Tasks=
-        echo PathOption=Cmd
-        echo SSHOption=OpenSSH
-        echo CRLFOption=CRLFAlways
-        echo BashTerminalOption=ConHost
-        echo PerformanceTweaksFSCache=Enabled
-        echo UseCredentialManager=Enabled
-        echo EnableSymlinks=Disabled
-        echo EnableBuiltinDifftool=Disabled
-    ) > config.inf
-
-    %installerName% /VERYSILENT /LOADINF="config.inf"
-    if errorlevel 1 (
-        echo Error during Git installation.
-        exit /b 1
-    )
-    del config.inf
-
-    :: Optional: Add Git to the system PATH
-    setx PATH "%PATH%;%installDir%\cmd"
+if %errorlevel% neq 0 (
+    echo Git is not installed. Please run gitinstaller.exe to install Git.
+    echo Press any key to exit...
+    pause >nul
+    exit /b 1
 ) else (
-    echo Git is already installed.
+    echo Git is installed.
 )
 
 :: Change to the game directory
