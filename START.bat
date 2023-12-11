ac5567bd04d57e10ab80714d3650798b6af17cc3
damianolszewski <44201801+damianolszewski@users.noreply.github.com>
2023-12-11 11:22:19 +0100

    start.bat update

--- START.bat
+++ START.bat
@@ -1,6 +1,19 @@
 @echo off
 setlocal
 
+echo:
+echo:
+echo              _    _               _ _          _             
+echo        /\   ^| ^|  ^| ^|             ^| (_)        ^| ^|            
+echo       /  \  ^| ^| _^| ^|_ _   _  __ _^| ^|_ ______ _^| ^|_ ___  _ __ 
+echo      / /\ \ ^| ^|/ / __^| ^| ^| ^|/ _` ^| ^| ^|_  / _` ^| __/ _ \^| '__^|
+echo     / ____ \^|   ^<^| ^|_^| ^|_^| ^| (_^| ^| ^| ^|/ / (_^| ^| ^|^| (_) ^| ^|   
+echo    /_/    \_\_^|\_\\__^|\__,_^|\__,_^|_^|_/___\__,_^|\__\___/^|_^|                        
+echo:
+echo:
+echo:
+echo:
+
 :: Define the game executable and repository URL
 set "gameExecutable=Lethal Company.exe"
 set "repositoryURL=https://github.com/damianolszewski/PaczkaDoLethala.git"
@@ -10,12 +23,12 @@ set "gitExecutable=git"
 :: Check if Git is installed by checking the version
 git --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Git is not installed. Please run gitinstaller.exe to install Git.
-    echo Press any key to exit...
+    echo Git nie jest zainstalowany!
+    echo Wcisnij dowolny przycisk aby wyjsc...
     pause >nul
     exit /b 1
 ) else (
-    echo Git is installed.
+    echo Git jest zainstalowany.
 )
 
 :: Change to the game directory
@@ -23,23 +36,39 @@ cd /d "%gameDirectory%"
 
 :: Initialize the directory as a Git repository if not already a repository
 if not exist ".git" (
-    echo Initializing repository in the current directory...
+    echo Inicjalizuje repozytorium w folderze gry...
+	echo -----------------------------------------------
     git init
     git remote add origin "%repositoryURL%"
+	echo -----------------------------------------------
     
-    echo Fetching repository data...
+    echo Pobieram informacje o repozytorium...
+	echo -----------------------------------------------
     git fetch
+	echo -----------------------------------------------
 
-    echo Checking out files from the repository...
+    echo Pobieram pliki z repozytorium...
+	echo -----------------------------------------------
     git checkout -t origin/main
+	echo -----------------------------------------------
 
-    echo Repository cloned into existing directory.
+    echo Repozytorium zostalo poprawnie sklonowane.
 ) else (
-    echo Repository already cloned. Pulling updates...
+    echo Repozytorium bylo juz klonowane. Pobieram aktualizacje...
+	echo -----------------------------------------------
     git pull origin main
+	echo -----------------------------------------------
 )
 
-echo Press any key to continue...
-pause 
+echo Pliki zaktualizowane.
+
+:: Check if a parameter was passed to the script
+if not "%~1"=="" (
+    echo Startuje gre...
+    %1
+) else (
+    echo Wcisnij dowolny przycisk aby wyjsc...
+    pause >nul
+)
 
 endlocal
